3c3c9fcfe4eaccd3b112c274c0317bae0f513eb4
David Requeno <davidrequeno52@gmail.com>
2023-04-24 21:26:28 -0600

    fetch

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,18 @@
-export default function IndexPage() {
+
+async function fetchUsers() {
+  const res = await fetch('https://reqres.in/api/users')
+  const data = await res.json()
+  return data.data
+}
+
+export default async function IndexPage() {
+  const users = await fetchUsers()
   return (
     <div>
       <h1>Hello index page</h1>
+      <div>
+        {JSON.stringify(users)}
+      </div>
     </div>
   )
 }
